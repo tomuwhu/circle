94e88488b80e76b8392d9cfa1ddb9960d76f2421
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-11-02 14:03:23 +0100

    .

--- src/App.svelte
+++ src/App.svelte
@@ -2,30 +2,35 @@
   const A = n => Array.from({length: n})
   const T = d => A(101).map(_ => A(101).fill(d))
   const t = T(0)
-  const circle = (cx, cy, r) => {
+  const circle = (cx, cy, r, c = 1) => {
     let x = 0
     let y = -r
     let p = -r
-    while (x < -y) {
+    while (x < - y) {
       if (p > 0) {
         y++
         p += 2 * (x + y) + 1
       } else {
         p += 2 * x + 1
       }
-      t[cx + x][cy + y] = 1
-      t[cx - x][cy + y] = 1
-      t[cx + x][cy - y] = 1
-      t[cx - x][cy - y] = 1
-      t[cx + y][cy + x] = 1
-      t[cx - y][cy + x] = 1
-      t[cx + y][cy - x] = 1
-      t[cx - y][cy - x] = 1
+      t[cx + x][cy + y] += c
+      t[cx + x][cy - y] += c
+      t[cx + y][cy + x] += c
+      t[cx - y][cy + x] += c
+      if (x>0) {
+        t[cx - x][cy + y] += c
+        t[cx + y][cy - x] += c
+        t[cx - y][cy - x] += c
+        t[cx - x][cy - y] += c
+      }
       x++
     }
   }
-for (let i = 1; i < 5; i++)
-  circle(50, 50, i*10 + 10)
+
+for (let i = 1; i <= 50; i++) {
+  circle(50, 50, i)
+}
+
 </script>
 
 <main>
@@ -52,5 +57,14 @@ for (let i = 1; i < 5; i++)
   }
   :global(td.p1) {
     background-color: white;
+  }
+  :global(td.p2) {
+    background-color: rgb(255, 200, 200);
+  }
+  :global(td.p3) {
+    background-color: rgb(200, 255, 249);
+  }
+  :global(td.p4) {
+    background-color: rgb(252, 255, 200);
   }
 </style>
